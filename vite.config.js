4dc637b80c6cef2cbd3204e24f50cc886926df86
deffryap <deffryap@gmail.com>
2025-08-07 01:34:17 +0700

    Update API

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   define: {
-    __API_URL__: JSON.stringify(process.env.VITE_API_URL || 'https://katalog-film-be-prisma.netlify.app/api')
+    __API_URL__: JSON.stringify(process.env.VITE_API_URL || 'https://katalog-film-be-prisma.netlify.app/api/movies')
   }
 })
